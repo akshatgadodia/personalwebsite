39a86ec2de6674c21a84e91d919ae074153b5891
Akshat Gadodia <akshatgadodia@gmail.com>
2022-05-28 00:47:51 +0530

    Contact Page Created

--- src/Abilities.js
+++ src/Abilities.js
@@ -2,7 +2,22 @@ import React from 'react'
 import './AbilitiesCSS.css'
 import {SkillsData,LanguagesData,ToolsData} from './Data/AbilitiesData.js'
 import DisplayAbility from './DisplayAbility';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import StarIcon from '@mui/icons-material/Star';
+
 const Abilities = () => {
+  const stars = (a) => {
+    var list=[];
+    var i;
+    for(i=0; i<a; i++) {
+      list.push(<StarIcon fontSize="small"/>);
+    }
+    for(i=0; i<5-a; i++) {
+      list.push(<StarOutlineIcon fontSize="small"/>);
+    }  
+    return list ;
+  }
+  
   return (
     <div className='abilities' id='Abilities-Page'>
       <h1>Abilities</h1>
@@ -12,21 +27,21 @@ const Abilities = () => {
       <h4>Skills</h4>
       <div className='abilities-tab-div'>
           {SkillsData.map((item,index)=>{
-              return(<DisplayAbility key={index} name={item.name} rating={item.rating}/>);
+              return(<DisplayAbility key={'A'+index} name={item.name} rating={stars(item.rating)}/>);
           })}
       </div>
       <hr />
       <h4>Languages</h4>
       <div className='abilities-tab-div'>
           {LanguagesData.map((item,index)=>{
-              return(<DisplayAbility key={index} name={item.name} rating={item.rating}/>);
+              return(<DisplayAbility key={'B'+index} name={item.name} rating={stars(item.rating)}/>);
           })}
       </div>
       <hr />
       <h4>Tools</h4>
       <div className='abilities-tab-div'>
           {ToolsData.map((item,index)=>{
-              return(<DisplayAbility key={index} name={item.name} rating={item.rating}/>);
+              return(<DisplayAbility key={'C'+index} name={item.name} rating={stars(item.rating)}/>);
           })}
       </div>
     </div>
--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,52 @@
 import React from 'react'
 import './ContactCSS.css'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import emailjs from 'emailjs-com';
+
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault(); 
+    emailjs.sendForm('gmail', 'personal_website_temp', e.target, 'MYI-Frv4UhKJuvMa7')
+      .then((result) => {
+         console.log(result.text);
+         window.location.reload();
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
     <div className='contact' id='Contact-Page'>
       <h1>Contact</h1>
       “If I had asked people what they wanted, they would have said faster horses. ”
        <br /> - Henry Ford
       <hr />
-
+      <div className='contact-div' >
+        <div className='contact-form'>
+          <form onSubmit={sendEmail}>
+            <input type="text" name='name' placeholder="Enter Your Name"/> <br />
+            <input type="email" name='email' placeholder="Enter Email"/> <br />
+            <input type='text' name='message' placeholder="Enter Message"/> <br />
+            <input type="submit" className='contact-form-submit'/>
+          </form>
+        </div>
+        <div className='contact-information'>
+          <a href="mailto:akshatgadodia@gmail.com"><LinkedInIcon fontSize="large" style={{color:"white"}}/> </a>
+          <a href="mailto:akshatgadodia@gmail.com"><GitHubIcon fontSize="large" style={{color:"white"}}/> </a>
+          <a href="mailto:akshatgadodia@gmail.com"><InstagramIcon fontSize="large" style={{color:"white"}}/> </a>
+          <a href="mailto:akshatgadodia@gmail.com"><FacebookIcon fontSize="large" style={{color:"white"}}/></a>
+          <a href="mailto:akshatgadodia@gmail.com"><AlternateEmailIcon fontSize="large" style={{color:"white"}}/> </a><br />
+          <br /><br />
+          Email :
+          <h4>akshatgadodia@gmail.com</h4><br />
+          Phone No. :
+          <h4>+91-7737152961</h4><br />
+        </div>
+      </div>
     </div>
   )
 }
